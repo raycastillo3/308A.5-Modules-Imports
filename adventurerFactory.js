8358b7d1e3cb305697636ca8967a767b0c450ea9
raycastillo3 <60750068+raycastillo3@users.noreply.github.com>
2024-11-05 17:13:11 -0500

    added the adventurerFactory module import

---
+++ adventurerFactory.js
@@ -0,0 +1,16 @@
+export default class AdventurerFactory {  
+    constructor (role) {
+      this.role = role;
+      this.adventurers = [];
+    }
+    generate (name) {
+      const newAdventurer = new Adventurer(name, this.role);
+      this.adventurers.push(newAdventurer);
+    }
+    findByIndex (index) {
+      return this.adventurers[index];
+    }
+    findByName (name) {
+      return this.adventurers.find((a) => a.name === name);
+    }
+  }
--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 import { adventurer as adventurerObj, Adventurer as AdventurerClass} from "./adventurer.js";
 import Character from "./character.js";
 import Companion from "./companion.js";
+import AdventurerFactory from "./adventurerFactory.js";
 
-// console.log(adventurerObj);
+console.log(adventurerObj);
 adventurerObj.roll(); 
 
 
 const robin = new Character("Robin");
-// console.log(robin);
+console.log(robin);
 
 //PART 3:
 const adventurer1 = new AdventurerClass("Robin", "Carpenter", "woodChooper", "Great with the axe");
-// console.log(adventurer1);
+console.log(adventurer1);
 
 
 const companion2 = new Companion("Leo", "cat");
-// console.log(companion2);
+console.log(companion2);
 
 const adventurer2 = new AdventurerClass("Robin", "Carpenter", "woodChopper", "Great with the axe", companion2);
-// console.log(adventurer2);
+console.log(adventurer2);
 
 // // const adventurer3 = new Adventurer("Bob", "Fighter", "Strong", "Great for lifting");
 
 
 // //PART 5:
-
-  
-//   const healers = new AdventurerFactory("Healer");
-//   const robin = healers.generate("Robin");
+  const healers = new AdventurerFactory("Healer");
+  console.log(healers);
 
 
 
